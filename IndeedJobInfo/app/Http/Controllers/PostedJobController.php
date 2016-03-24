65086b094cd01cdce0a07b52ff26b4ee14d126c0
curogihu <curoas@yahoo.co.jp>
2016-03-24 01:23:52 -0700

    delete comment

--- IndeedJobInfo/app/Http/Controllers/PostedJobController.php
+++ IndeedJobInfo/app/Http/Controllers/PostedJobController.php
@@ -12,62 +12,47 @@ class PostedJobController extends Controller
 {
 
   public function extract(){
-    // HTMLデータを取得する
+    //America/Vancouver
+    date_default_timezone_set('America/Vancouver');
+    $cnt = 0;
 
     // min = 0, max = 990
+    for($i = 0; $i < 990; $i += 10){
+      $html = file_get_html('http://ca.indeed.com/jobs?q=php&sort=date&start=' . $i);
 
-    $html = file_get_html('http://ca.indeed.com/jobs?q=php&sort=date&start=0');
+      foreach ($html->find('div[class=row  result]') as $elem) {
+        $ret = $elem->find('span[itemprop=name]', 0);
 
-    foreach ($html->find('div[class=row  result]') as $elem) {
-      $ret = $elem->find('span[itemprop=name]', 0);
+        // company name
+        if(isset($ret)){
+          $link = "http://ca.indeed.com" . $elem->find('a[rel=nofollow]', 0)->href;
+          $company = Company::where('Link', $link)->get();
 
-      // company name
-      if(isset($ret)){
-        $regionName = $elem->find('span[itemprop=addressLocality]', 0)->plaintext;
-        $regionArr = explode(',', str_replace(" ", "", $regionName));
+          // prevent from inputting duplicate record
+          if($company->isEmpty()){
+            $regionName = $elem->find('span[itemprop=addressLocality]', 0)->plaintext;
+            $regionArr = explode(',', str_replace(" ", "", $regionName));
 
-        $company = new Company;
+            $company = new Company;
 
-        $company->CompanyName = trim($elem->find('span[itemprop=name]', 0)->plaintext);
-        $company->City = $this->getCityName($regionArr);
-        $company->Province = $this->getProvinceName($regionArr);
-        $company->Link = $elem->find('a[rel=nofollow]', 0)->href;
+            $company->CompanyName = trim($elem->find('span[itemprop=name]', 0)->plaintext);
+            $company->City = $this->getCityName($regionArr);
+            $company->Province = $this->getProvinceName($regionArr);
+            $company->JobTitle = $elem->find('a[rel=nofollow]', 0)->title;
+            $company->Link = "http://ca.indeed.com" . $elem->find('a[rel=nofollow]', 0)->href;
 
-        $company->AddedTime = date("Ymd");
+            $company->AddedTime = date("Ymd");
 
-        $company->save();
+            $company->save();
+            $cnt++;
+          }
+        }
       }
 
-/*
-      // company address
-      $regionName = $elem->find('span[itemprop=addressLocality]', 0);
-      $regionArr = explode(',', str_replace(" ", "", $regionName));
-
-      echo "----------------------------------------------<br>";
-      echo "region name = " . $this->getCityName($regionArr) . " " . $this->getProvinceName($regionArr) . "<br>";
-
-      echo $this->getCityName($regionArr) . "<br>";
-      echo $this->getProvinceName($regionArr) . "<br>";
-
-      // Find all links
-      echo $elem->find('a[rel=nofollow]', 0)->href . "<br>";
-
-      // http://ca.indeed.com/ . [a href]
-
-      $easilyApply = false;
-      $sponsered = false;
-
-      foreach($elem->find('span[class=iaLabel]') as $easilyApply){
-        echo $easilyApply->plaintext . "<br>";
-      }
-
-      foreach($elem->find('span[class=sdn]') as $sponsered){
-        echo $sponsered->plaintext . "<br>";
-      }
-
-      echo "-----------------------<br>";
-      */
+      sleep(3);
     }
+
+    echo "Add cnt = " . $cnt;
   }
 
   public function getCityName($pArr){
